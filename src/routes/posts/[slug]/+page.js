9bc5438939af283c44df22ec0cf6739b6311ca9f
alexjeffery271 <122819916+alexjeffery-verse@users.noreply.github.com>
2023-01-18 22:44:48 +0000

    add paragraph testimonials to posts

--- src/routes/posts/[slug]/+page.js
+++ src/routes/posts/[slug]/+page.js
@@ -1,16 +1,30 @@
 let data;
-let included;
+let includes;
+let filteredCategories;
+let testimonials;
 
-export async function load( {fetch, params} ) {
+export async function load( { fetch, params } ) {
   let request = "http://localhost:8888/web/jsonapi/node/post";
   request += '/' + params.slug;
-  request += "?include=field_category";
+  request += "?include=field_category,field_paragraph";
   
   const fetchData = await fetch( request, {} )
-    .then(response => response.json())
-    .then(res => {
-      data = res.data
-      included = res.included
+    .then( response => response.json() )
+    .then( res => {
+      data = res.data;
+      includes = res.included;
+
+      let categories = includes.filter(function(category) {
+        return category.type == 'taxonomy_term--category'
+      });
+
+      filteredCategories = categories.map(({ attributes: { name } }) => {
+        return name;
+      });
+
+      testimonials = includes.filter(function(testimonial) {
+        return testimonial.type == 'paragraph--testimonials'
+      });
     })
     .catch(error => {
     console.log(error);
@@ -23,11 +37,8 @@ export async function load( {fetch, params} ) {
       content: data.attributes.body.value
     },
     included: {
-      categoryNames: included.map(({ attributes: { name } }) => {
-        return {
-          name
-        };
-      })
+      categories: filteredCategories,
+      testimonials: testimonials
     }
   }
 }
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,7 +1,12 @@
 <script>
 export let data;
+
 let post = data.data;
-let categories = data.included.categoryNames
+let categories = data.included.categories
+let testimonials = data.included.testimonials
+
+console.log(testimonials)
+
 </script>
 
 <h1>{post.title}</h1>
@@ -10,14 +15,25 @@ let categories = data.included.categoryNames
 	<p>
 		Categories: 
 		{#each categories as category }
-			<span>{category.name}</span>
+			<span>{category}</span>
 		{/each}
 	</p>
 	{:else}
-    <p class="loading">unassigned category</p>
+    <p>Unassigned Category</p>
 {/if}
 
 <p>{post.content}</p>
+
+<h2>Testimonials</h2>
+{#if testimonials }
+	<div>
+		{#each testimonials as testimonial }
+			<h3>{testimonial.attributes.field_author_title.value}</h3>
+			<p>{testimonial.attributes.field_text.value}</p>
+			<p>{testimonial.attributes.field_author_name.value}</p>
+		{/each}
+	</div>
+{/if}
 <a href='/posts'>See all the posts</a>
 
 <style>
